5baec42f6cdf39b35e923de75a630f24cc7165f1
onluwin <nikitagurzhiy1@gmail.com>
2022-08-29 01:30:12 +0300

    .

---
+++ test.js
@@ -0,0 +1,63 @@
+// console.log(Math.PI.toFixed(2))
+// console.log(Number(Math.PI.toFixed(2)))
+
+
+
+// const base = Number(prompt('your number'));
+// const power = Number(prompt('your power'));
+// console.log(base ** power);
+
+
+
+// const max = 100;
+// const min = 1;
+// console.log(Math.random() * (max - min) + min)
+
+
+// let msg = prompt('введите ваше имя');
+// const yn = ('ваше имя:');
+// console.log(yn, msg);
+
+
+
+// let balance = 1000;
+// const payment = 200;
+// let result = balance - payment;
+// const message = `Общая стоимость заказа` + ' ' + payment + ' ' + 'кредитов. Проверяем кол-во доступных средств на счету.';
+// const succesMessage = "Операция завершена"
+
+// console.log('На вашем счету ' + balance + ' кредитов')
+// console.log(message)
+
+// if (balance > payment) {
+//     balance - payment
+//     console.log('На счету осталось ' + result + ' кредитов')
+//     console.log(succesMessage)
+// } else {
+//     console.log('На счету недостаточно средств для проведения операции!');
+// }
+
+
+
+// const totalSpent = 2000;
+// let payment = 99;
+// let discount = 0;
+// let message;
+
+// if (payment >= 100) {
+//     discount = 2;
+// } else (payment < 1000) 
+
+// if (payment >= 1000) {
+//     discount = 5;
+// } else (payment <= 5000);
+
+// if (payment >= 5000) {
+//     discount = 10;
+// } else (payment < 100); {
+//     discount;
+// }
+
+// console.log('Оформляем заказ на сумму ' + payment + ' ' + 'со скидкой' + ' ' + discount + '%')
+
+
