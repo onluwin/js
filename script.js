05a609ac7ce0d89800ff6395c23c2ab968023a3b
onluwin <nikitagurzhiy1@gmail.com>
2022-09-12 17:04:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1466,3 +1466,28 @@
 // const tomorrowIcon = tomorrowIcon1;
 
 // console.log(highToday, tomorrowHigh, todayIcon1); НЕ ДОПИСАНО (3 mod, задача 26 вроде)
+
+
+
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+
+//     let totalPrice = 0;
+//     for (let product of products) {
+//         if (product.name === productName) {
+//     totalPrice = product.price * product.quantity;
+//     }
+// }
+// return totalPrice
+
+// }
+// console.log(calculateTotalPrice('Radar'));
+
+
+
