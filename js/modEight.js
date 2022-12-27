9fe89396a1ccb4d23e8f371bbcb16ec7d5a0ae5c
onluwin <nikitagurzhiy1@gmail.com>
2022-12-27 13:37:49 +0300

    .

---
+++ js/modEight.js
@@ -0,0 +1,24 @@
+// RANDOMIZER
+
+// const minNumberInput = document.querySelector('.min');
+// const maxNumberInput = document.querySelector('.max');
+// const resultBtn = document.querySelector('.result-btn');
+// const resultAPI = document.querySelector('.result-js');
+
+// let minValue = null;
+// let maxValue = null;
+
+// let result = 0
+
+// const onNumberInput = function (e) {
+//     minValue = Number(minNumberInput.value);
+//     maxValue = Number(maxNumberInput.value);
+//     result = Math.floor(Math.random() * maxValue) + minValue;
+// }
+// const onResultBtnClick = () => {
+//     resultAPI.textContent = result;
+// }
+
+// minNumberInput.addEventListener('input', onNumberInput)
+// maxNumberInput.addEventListener('input', onNumberInput)
+// resultBtn.addEventListener('click', onResultBtnClick)
---
+++ js/modNine.js
@@ -0,0 +1,99 @@
+// const condition = Math.random() > 0.3
+// const promise = new Promise((resolve, reject) => {
+//     if (condition) {
+//         resolve('все гуд')
+//     }
+//     reject('reject')
+// })
+// promise.then(v => console.log(v)).catch(e=>console.log(e))
+
+
+
+// const fetchUserById = (username, id) => {
+//     const condition = Math.random() > 0.5
+//     return new Promise((resolve, reject) => {
+//         if (condition) {
+//             resolve(`нашел пользователя ${username} по айди ${id}`)
+//         }
+//         reject(`пользователя с ником ${username} и айди ${id} не найдено`)
+//     })
+
+// }
+// fetchUserById('mango', 10).then(v=> console.log(v)).catch(error=>console.log(error))
+
+
+
+// const targetDate = new Date(2021, 1, 21, 3, 12)
+// function getWeekDay (date) {
+//     const days = ['вс', 'пн', 'вт', 'ср', 'чт', 'пятница', 'сб'];
+//     return days[targetData.getDay(date)]
+// }
+// console.log(getWeekDay(targetDate));
+
+
+
+// const tagPRef = document.querySelector('.promise-result');
+// const promiseBtn = document.querySelector('.promise-btn');
+
+// const getRandomNumber = () => Math.floor(Math.random() * 4) + 1;
+
+// let delay = 0;
+
+// const createPromise = (number, delay) => {
+//     delay = number * 1000
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (number < 3) {
+//                 resolve(delay);
+//             }
+//             reject(delay);
+//         }, delay);
+
+//     })
+// }
+
+// const onCreatePromiseBtnClick = () => {
+//     tagPRef.textContent = 'Promise pending . . .'
+//     createPromise(getRandomNumber(), delay).then(delay => { tagPRef.textContent = `✅Resolved after ${delay} ms` }).catch(delay => { tagPRef.textContent = `❌Rejected after ${delay} ms` })
+// }
+
+// promiseBtn.addEventListener('click', onCreatePromiseBtnClick)
+
+
+// const frameworks = ['React', 'Vue', 'Angular'];
+// const getRandomDelay = () => Math.floor(Math.random() * 3); тогда delay = getRandomDelay() * 1000;
+// const getRandomDelay = () => Math.floor(Math.random() * 2000);
+// const isSucces = () => Math.floor(Math.random() * 2) + 1;
+
+// const makePromise = (framework) => {
+//     return new Promise((resolve, reject) => {
+//         const delay = getRandomDelay();
+//         setTimeout(() => {
+//             if (isSucces() < 2) {
+//                 resolve({ message: 'Promise resolve✅', framework, delay })
+//             }
+
+//             reject({ message: 'Promise error❌', framework, delay })
+//         }, delay);
+//     })
+// }
+
+// если без Promise.race()
+
+// frameworks.map(item => {
+//     makePromise(item).then(v => {
+//         console.log(v.message,v.framework, v.delay);
+//     }).catch(e => {
+//         console.log(e.message, e.framework, e.delay);
+//     })
+// })
+
+
+// это гуд 🔽
+
+// const promises = frameworks.map(makePromise);
+// Promise.race(promises).then(v => { console.log(v.message, v.framework, v.delay) }).catch(e => { console.log(e.message, e.framework, e.delay) })
+
+// Promise.all(promises).then(data => {
+//     data.map(v => { console.log(v.message, v.framework, v.delay) })
+// }).catch(e => { console.log(e.message, e.framework, e.delay) })
