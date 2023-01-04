c51ed78321aefecc1e57cbe63ecdda911a7e0b9b
onluwin <nikitagurzhiy1@gmail.com>
2023-01-04 23:57:31 +0300

    mod 10

---
+++ js/modTen.js
@@ -0,0 +1,7 @@
+
+// const VLAD_API_KEY = '1ebe2741fc2dad72428940b7d498d033';
+// fetch(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${VLAD_API_KEY}`).then(r => r.json()).then(data => {
+//     console.log('это then а это data ->', data);
+// }).catch(e => {
+// console.log('это catch а это error ->', e)})
+
