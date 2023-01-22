08ac040d1d93d53f9d82e730af93cf1c793fa960
onluwin <nikitagurzhiy1@gmail.com>
2023-01-22 17:44:42 +0300

    add CodeWars.js

---
+++ js/codeWars.js
@@ -0,0 +1,20 @@
+// function spinWords(str) {
+//     const reversedWords = []
+//     str.split(' ').forEach(word => {
+//         if (word.length < 5) {
+//             reversedWords.push(word)
+//         } else {
+//             reversedWords.push(word.split('').reverse().join(''))
+//         }
+        
+//     });
+//     return reversedWords.join(' ')
+// }
+// console.log(spinWords('Hey fellow warriors'));
+
+
+
+// function solution(str){
+//     return str.split('').reverse().join('')
+// }
+// console.log(solution('word'));
