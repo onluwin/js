7a1b3cbe62104eec78c235fa96c7f17f2d27d28c
onluwin <nikitagurzhiy1@gmail.com>
2023-03-13 18:25:44 +0200

    Update codeWars.js

--- js/codeWars.js
+++ js/codeWars.js
@@ -6,21 +6,19 @@
 //         } else {
 //             reversedWords.push(word.split('').reverse().join(''))
 //         }
-        
+
 //     });
 //     return reversedWords.join(' ')
 // }
 // console.log(spinWords('Hey fellow warriors'));
 
-
-
+// ANOTHER
 // function solution(str){
 //     return str.split('').reverse().join('')
 // }
 // console.log(solution('word'));
 
-
-
+// ANOTHER
 // function persistence(num) {
 //     if (String(num).length === 1 || !num) {
 //         return 0
@@ -36,8 +34,7 @@
 // }
 // console.log(persistence(999));
 
-
-
+// ANOTHER
 // function makeNegative(num) {
 //     if (!num) return;
 //     let result;
@@ -49,5 +46,38 @@
 // }
 // console.log(makeNegative(3));
 
+// ANOTHER
+// function sum(numbers) {
+//   return numbers.reduce((acc, item) => {
+//     console.log('acc', acc);
+//     if (typeof item !== 'number') {
+//       return 0;
+//     }
+//     acc += item;
+//     return acc;
+//   }, 0);
+// }
 
+// const sum = numbers =>
+//   numbers.reduce(
+//     (acc, item) => (typeof item === 'number' ? (acc += item) : 0),
+//     0,
+//   );
 
+// console.log(sum([]), sum([1, 5.2, 4, 0, -1]));
+
+// ANOTHER
+// function getCount(str) {
+//   const vowels = ['a', 'e', 'i', 'o', 'u'];
+//   const arrayOfStr = str.split('');
+//   let answer = 0;
+//   vowels.forEach(letter => {
+//     for (let i = 0; i < arrayOfStr.length; i += 1) {
+//       if (letter === arrayOfStr[i]) {
+//         answer += 1;
+//       }
+//     }
+//   });
+//   return answer;
+// }
+// console.log(getCount('abracadabra')); // 5
